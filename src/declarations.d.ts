44dcdd63dd323ed684aa56b9680a86a7ed477293
Matheus Couto <couto.matheus@outlook.com>
2018-09-02 10:30:40 -0300

    ts: Post and Comment type declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,39 @@
+export interface Post {
+    // Identificador único
+    id: string,
+    // Data de criação - dados default rastreiam isto em Unix time. 
+    // Você pode usar Date.now() para obter este número
+    timestamp: number,
+    // Título do post
+    title: string,
+    // Corpo do post
+    body: string,
+    // Autor do post
+    author: string,
+    // Deve ser uma das categorias fornecidas pelo servidor
+    category: string,
+    // Votos líquidos que a postagem recebeu (default: 1)
+    voteScore?: number,
+    // Marcado se o post foi 'deletado' (sem acesso no front end), (default: false)
+    deleted?: boolean,
+}
+
+export interface Comment {
+    // Identificador único
+    id: string,
+    // id do post pai
+    parentId: string,
+    // Data de criação - dados default rastreiam isto em Unix time. 
+    // Você pode usar Date.now() para obter este número
+    timestamp: number,
+    // Corpo do comentário
+    body: string,
+    // Autor do comentário
+    author: string,
+    // Votos líquidos que a postagem recebeu (default: 1)
+    voteScore?: number,
+    // Marcado se o post foi 'deletado' (sem acesso no front end), (default: false)
+    deleted?: boolean,
+    // Marcado quando o post pai foi deletado, mas o comentário em si não foi.
+    parentDeleted?: boolean,
+}
